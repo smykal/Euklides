57e58f95257c27825fd2ef4d2489516a1b94fc5d
Mateusz <smykal@engineer.com>
2021-07-20 20:38:43 +0200

    solution

--- src/main/java/com/engine/Ekluides.java
+++ src/main/java/com/engine/Ekluides.java
@@ -3,39 +3,23 @@ package com.engine;
 import java.util.Scanner;
 
 public class Ekluides {
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("please give number one");
-        double numberA = scanner.nextDouble();
-        System.out.println("please give number one");
-        double numberB = scanner.nextDouble();
-        double greatestCommonDivisor = 1;
-        if (numberB == numberB) {
-            System.out.println("greatest common divisor is: " + numberA);
-        } else {
+        System.out.println("podaj większą liczbę");
+        int a = scanner.nextInt();
+        System.out.println("podaj mniejszą liczbę");
+        int b = scanner.nextInt();
 
-            do {
-                if (numberAIsBiggerThanNumberB(numberA, numberB)) {
-                    greatestCommonDivisor = divideNumbersWithModuloAsResult(numberA, numberB);
-                } else {
-                    greatestCommonDivisor = divideNumbersWithModuloAsResult(numberB, numberA);
-                }
-
-            } while (greatestCommonDivisor == 0);
-        }
-
+        System.out.println("NWD dla " + a + " i " + b + " to " + nwd(a, b));
     }
 
-
-    private static double divideNumbersWithModuloAsResult(double numberOne, double numberTwo) {
-        return numberOne % numberTwo;
+    private static int nwd(int a, int b) {
+        if (b == 0)
+            return a;
+        return nwd(b, a % b);
     }
 
-    private static boolean numberAIsBiggerThanNumberB(double numberA, double numberB) {
-        if (numberA > numberB) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
+
